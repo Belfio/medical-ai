0d35c316455154529e5dcb2f1f447f579dcfbde8
albelfio <5812464+Belfio@users.noreply.github.com>
2024-09-13 20:11:08 +0100

    x

--- app/routes/diseases.tsx
+++ app/routes/diseases.tsx
@@ -1,14 +1,8 @@
-import { Link } from "@remix-run/react";
+import { categories, diseases } from "~/lib/const";
 
 export default function DiseasesPage() {
   return (
     <div>
-      <div className="mt-8 flex gap-4">
-        <h2 className="text-2xl font-semibold mb-4">Available Models</h2>
-        <Link to="/model-add">
-          <Button>Add dataset</Button>
-        </Link>
-      </div>
       Diseases
       <div className="flex flex-col gap-4">
         {diseases.map((disease) => (
