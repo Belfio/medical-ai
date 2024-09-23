8b26206e9e4df146e1b352126feef12c9a6d953e
albelfio <5812464+Belfio@users.noreply.github.com>
2024-09-23 23:23:14 +0100

    cleaned a file

--- app/routes/models.add.tsx
+++ app/routes/models.add.tsx
@@ -1,28 +1,10 @@
-import {
-  ActionFunctionArgs,
-  unstable_parseMultipartFormData,
-  UploadHandler,
-  UploadHandlerPart,
-} from "@remix-run/node";
-import { ModelType } from "~/lib/types";
-import {
-  Form,
-  json,
-  redirect,
-  useActionData,
-  useFetcher,
-  useLoaderData,
-  useNavigation,
-  useSubmit,
-} from "@remix-run/react";
+import { useActionData, useFetcher, useLoaderData } from "@remix-run/react";
 import db from "~/lib/db";
 import { Button } from "~/components/ui/button";
 import { useState } from "react";
 
-import { s3UploaderHandler } from "~/upload.server";
-
 import { AlertCircle } from "lucide-react";
-import { randomId } from "~/lib/utils";
+
 import Upload from "~/components/Upload";
 import { Alert, AlertTitle, AlertDescription } from "~/components/ui/alert";
 import ModelQuestionnaire from "~/components/ModelQuestionnaire";
@@ -30,7 +12,7 @@ import ModelUploadSmall from "~/components/UploadSmall";
 
 export default function ModelAdd() {
   const fetcher = useFetcher();
-  const error = useActionData<typeof action>() as {
+  const error = useActionData<typeof error>() as {
     error: string;
     missingFields: string[];
   };
@@ -42,15 +24,8 @@ export default function ModelAdd() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget as HTMLFormElement);
-    // Print formData values
-    for (const [key, value] of formData.entries()) {
-      console.log(`${key}: ${value}`);
-    }
-
     if (!files) return;
-
     Array.from(files).map((file) => formData.append(file.name, file));
-
     console.log("sending files?", files.length);
     await fetcher.submit(formData, {
       method: "post",
@@ -58,6 +33,7 @@ export default function ModelAdd() {
       action: "/models/add/file",
     });
   };
+
   return (
     <div className="flex flex-col gap-4 max-w-[540px]">
       <h1>Upload your model</h1>
@@ -136,84 +112,4 @@ export const loader = async () => {
   const diseases = await db.disease.getByLatest(100);
   console.log("datasets length", datasets.length);
   return { datasets, diseases };
-};
-
-export const action = async ({ request }: ActionFunctionArgs) => {
-  const formDataa = await request.formData();
-  console.log("formData", formDataa);
-  return json({ error: "test" }, { status: 400 });
-
-  const modelId = randomId();
-  console.log("model id", modelId);
-  const s3uploaderWithId: UploadHandler = (props: UploadHandlerPart) =>
-    s3UploaderHandler(props, modelId);
-  const formData = await unstable_parseMultipartFormData(
-    request,
-    s3uploaderWithId
-  );
-  const notebookFile = formData.get("notebookFile");
-  const modelFile = formData.get("modelFile");
-  console.log("modelFile", modelFile);
-  console.log("formData", formData);
-  console.log("notebookFile", notebookFile);
-  return json({ error: "test" }, { status: 400 });
-  const name = formData.get("name");
-  const description = formData.get("description");
-  const author = formData.get("author");
-  const website = formData.get("website");
-  const size = formData.get("size");
-  const diseaseIds = formData.get("diseaseId");
-  const diseaseCategories = formData.get("diseaseCategories");
-  const bodyParts = formData.get("bodyParts");
-  // const modelFile = formData.get("modelFile");
-  const dataType = formData.get("dataType");
-  const datasetIds = formData.get("datasetIds");
-  if (
-    !name ||
-    !description ||
-    !author ||
-    !notebookFile ||
-    !diseaseIds ||
-    !diseaseCategories ||
-    !dataType ||
-    !datasetIds
-  ) {
-    const missingFields = [];
-    if (!name) missingFields.push("name");
-    if (!description) missingFields.push("description");
-    if (!author) missingFields.push("author");
-    if (!diseaseIds) missingFields.push("diseaseIds");
-    if (!diseaseCategories) missingFields.push("diseaseCategories");
-    if (!notebookFile) missingFields.push("notebookFile");
-    if (!dataType) missingFields.push("dataType");
-    if (!datasetIds) missingFields.push("datasetIds");
-    return json(
-      { error: `Missing fields: ${missingFields.join(", ")}` },
-      { status: 400 }
-    );
-  }
-
-  // create the object dataset to save in the table dataset
-  const model: ModelType = {
-    modelId: modelId,
-    name: name as string,
-    description: description as string,
-    createdAt: new Date().toISOString(),
-    ranking: 0,
-    notebookFile: notebookFile as string,
-    modelFile: modelFile as string,
-    website: website as string,
-    tConst: "metadata",
-    diseaseIds: diseaseIds as string,
-    bodyParts: bodyParts as string,
-    userId: "1",
-    author: author as string,
-    size: size as string,
-    datasetIds: datasetIds as string,
-    diseaseCategory: diseaseCategories as string,
-    dataType: dataType as string,
-  };
-  await db.model.create(model);
-
-  return redirect("/models");
 };
